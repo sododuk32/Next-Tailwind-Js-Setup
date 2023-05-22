f74f3dfe728429911458efb4a34bdc1f907ebce1
dy k <37325163+sododuk32@users.noreply.github.com>
2023-05-22 16:08:00 +0900

    init

---
+++ app/page.js
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className='p-10 text-rose-500 text-1xl'>
+      hello
+    </div>
+  )
+}
---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+  "./app/**/*.{js,jsx}","./pages/**/*.{js,jsx}","./components/**/*.{js,jsx}"
+
+  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
